748a00ab599f298cdd4b26471edb9bfbb3a7dd91
Riyan Hermawan <riyanhrmwn@gmail.com>
2022-06-27 16:41:35 +0700

    create Api/AuthPassportController

---
+++ app/Http/Controllers/Api/V1/AuthPassportController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\TokenRepository;
+use Laravel\Passport\RefreshTokenRepository;
+
+class AuthPassportController extends Controller
+{
+    /**
+     * Register user.
+     *
+     * @return json
+     */
+    public function register(Request $request)
+    {
+        $input = $request->only(['name', 'email', 'password']);
+
+        $validate_data = [
+            'name' => 'required|string|min:4',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ];
+
+        $validator = Validator::make($input, $validate_data);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Please see errors parameter for all errors.',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password'])
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User registered succesfully, Use Login method to receive token.'
+        ], 200);
+    }
+
+    /**
+     * Login user.
+     *
+     * @return json
+     */
+    public function login(Request $request)
+    {
+        $input = $request->only(['email', 'password']);
+
+        $validate_data = [
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ];
+
+        $validator = Validator::make($input, $validate_data);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Please see errors parameter for all errors.',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        // authentication attempt
+        if (auth()->attempt($input)) {
+            $token = auth()->user()->createToken('passport_token')->accessToken;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User login succesfully, Use token to authenticate.',
+                'token' => $token
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'User authentication failed.'
+            ], 401);
+        }
+    }
+
+    /**
+     * Access method to authenticate.
+     *
+     * @return json
+     */
+    public function userDetail()
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Data fetched successfully.',
+            'data' => auth()->user()
+        ], 200);
+    }
+
+    /**
+     * Logout user.
+     *
+     * @return json
+     */
+    public function logout()
+    {
+        $access_token = auth()->user()->token();
+
+        // logout from current device only
+        $tokenRepository = app(TokenRepository::class);
+        $tokenRepository->revokeAccessToken($access_token->id);
+
+        // use this method to logout from all devices
+        // $refreshTokenRepository = app(RefreshTokenRepository::class);
+        // $refreshTokenRepository->revokeRefreshTokensByAccessTokenId($$access_token->id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User logout successfully.'
+        ], 200);
+    }
+}
